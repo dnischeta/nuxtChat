ec51a9b1eeb0f9c741b0dee89850c97d79245288
Dmitry <root@Dmitry99.org>
2019-12-24 23:36:41 +0300

    Created template and configured connection

---
+++ pages/index.vue
@@ -0,0 +1,26 @@
+<template>
+  <v-layout column justify-center align-center>
+    <v-flex xs12 sm8 md6> </v-flex>
+    <v-btn @click="message">
+      New Message 
+    </v-btn>
+  </v-layout>
+</template>
+
+<script>
+export default {
+  sockets: {
+    connect() {
+      console.log('Client IO connected');
+    }
+  },
+
+  methods: {
+    message() {
+      this.$socket.emit('createMessage', {
+        text: 'fromClient'
+      })
+    }
+  }
+};
+</script>
