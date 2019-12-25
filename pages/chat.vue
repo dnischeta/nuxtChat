5a4afc6af2741955b170c194608186a3aca153b1
Dmitry <root@Dmitry99.org>
2019-12-25 23:40:55 +0300

    Basic front and state for user

---
+++ pages/chat.vue
@@ -0,0 +1,17 @@
+<template>
+  <h1>Chat page {{ user.name }}</h1>
+</template>
+
+<script>
+import { mapState } from "vuex";
+
+export default {
+  head() {
+    return {
+      title: `Комната ${this.user.room}`
+    };
+  },
+  middleware: ["chat"],
+  computed: mapState(["user"])
+};
+</script>
--- pages/index.vue
+++ pages/index.vue
@@ -1,26 +1,83 @@
 <template>
   <v-layout column justify-center align-center>
-    <v-flex xs12 sm8 md6> </v-flex>
-    <v-btn @click="message">
-      New Message 
-    </v-btn>
+    <v-flex xs12 sm8>
+      <v-card min-width="400px">
+        <v-card-title><h2>Nuxt чат</h2></v-card-title>
+        <v-card-text>
+          <v-form ref="form" v-model="valid" lazy-validation>
+            <v-text-field
+              v-model="name"
+              :counter="16"
+              :rules="nameRules"
+              label="Ваше имя"
+              required
+            ></v-text-field>
+
+            <v-text-field
+              v-model="room"
+              :rules="roomRules"
+              label="Введите комнату"
+              required
+            ></v-text-field>
+
+            <v-btn
+              :disabled="!valid"
+              color="primary"
+              class="mr-4"
+              @click="submit"
+            >
+              Войти
+            </v-btn>
+          </v-form>
+        </v-card-text>
+      </v-card>
+    </v-flex>
   </v-layout>
 </template>
 
 <script>
+import { mapMutations } from 'vuex';
+
 export default {
-  sockets: {
-    connect() {
-      console.log('Client IO connected');
-    }
+  layout: "empty",
+
+  head: {
+    title: 'Добро пожаловать в Nuxt чат'
   },
 
+  sockets: {
+    connect() {
+      console.log("Client IO connected");
+    }
+  },
+  data: () => ({
+    valid: true,
+    name: "",
+    nameRules: [
+      v => !!v || "Введите имя",
+      v => (v && v.length <= 16) || "Имя не должно превышать 16 символов"
+    ],
+    room: "",
+    roomRules: [v => !!v || "Введите комнату"]
+  }),
+
   methods: {
-    message() {
-      this.$socket.emit('createMessage', {
-        text: 'fromClient'
-      })
+    ...mapMutations(['setUser']),
+    submit() {
+      if (this.$refs.form.validate()) {
+        const user = {
+          name: this.name,
+          room: this.room
+        }
+        this.setUser(user);
+        this.$router.push("/chat");
+      }
     }
   }
 };
 </script>
+
+
+
+
+
